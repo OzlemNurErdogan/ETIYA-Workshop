4b0f5753c6518662477fee51604a183550a43f19
Özlem Erdoğan <90027732+OzlemNurErdogan@users.noreply.github.com>
2022-11-17 10:15:59 +0000

    soru 4 eklendi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("****soru 1******");
 function findPrime(...numbers) {
 
   for (var i = 0; i < numbers.length; i++) {
@@ -19,3 +20,22 @@ function findPrime(...numbers) {
   }
 }
 findPrime(1,2,3,4, 120, 16, 13, 5);
+
+console.log("****soru 4******");
+let count=0
+let i,number 
+for(number=2;number<=1000;number++)
+{
+for( i=1;i<=number;i++)
+{
+  if(number%i==0)
+  count++
+  
+}
+
+if(count==2)
+
+console.log(number)
+count=0 
+}
+
